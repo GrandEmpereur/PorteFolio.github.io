ed7808e22256c39f40074bebe035d5dee07ffd53
Patoche <bartosikpatrick2@gmail.com>
2022-01-14 16:07:37 +0100

    :rocket:

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# Bienvenue sur mon porfolio
+
+vous allez devoir installer sass affin de pouvoir utiliser le code
+
+## Install mac : 
+```
+brew install sass/sass/sass
+ou
+npm install -g sass
+```
+
+## Install windows 
+
+```
+choco install sass
+or
+npm install -g sass
+```
+
+## Install linux
+
+```
+npm install -g sass
+```
---
+++ js/main.js
@@ -0,0 +1,116 @@
+//Burger
+let burger = document.getElementById('burger');
+let burgerLinks = document.getElementById('burgerLinks');
+
+function functionBurger() {
+    burgerLinks.classList.toggle('menuOpen')
+};
+
+burger.addEventListener('click', functionBurger)
+//Burger
+
+
+// Paralaxe
+const translate = document.querySelectorAll(".translate");
+const big_title = document.querySelector(".big-title");
+const social_icons = document.querySelector(".social-icon");
+const header = document.querySelector("header");
+const shadow = document.querySelector(".shadow");
+const content = document.querySelector(".content");
+const section = document.querySelector("section");
+const image_container = document.querySelector(".imgContainer");
+const opacity = document.querySelectorAll(".opacity");
+const border = document.querySelector(".border");
+
+let header_height = header.offsetHeight;
+let section_height = section.offsetHeight;
+
+window.addEventListener('scroll', () => {
+    let scroll = window.pageYOffset;
+    let sectionY = section.getBoundingClientRect();
+    
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateY(${scroll * speed}px)`;
+    });
+
+    opacity.forEach(element => {
+        element.style.opacity = scroll / (sectionY.top + section_height);
+    })
+
+    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    shadow.style.height = `${scroll * 0.5 + 300}px`;
+
+    social_icons.style.opacity = - scroll / (header_height / 2) + 1;
+
+})
+
+// Nav change active classe 
+let Navbar = document.querySelectorAll('.Nav')
+Navbar.forEach(function(change) {
+
+    change.addEventListener('clike', function() {
+
+      
+      NavList.classList.remove('Nav-active')
+      this.classList.add('NavList')
+      console.log(NavList)
+    })
+  
+
+})
+
+
+// Slider 
+    var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
+
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
+
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 10000);
+      }
+      repeater();
+    }
+    repeat();
