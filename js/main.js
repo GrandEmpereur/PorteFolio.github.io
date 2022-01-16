f513b2e240dc25879e9f05618201567968dc4666
Patoche <bartosikpatrick2@gmail.com>
2022-01-16 16:52:47 +0100

    :memo: Add contact & project page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-//Burger
-let burger = document.getElementById('burger');
-let burgerLinks = document.getElementById('burgerLinks');
-
-function functionBurger() {
-    burgerLinks.classList.toggle('menuOpen')
-};
-
-burger.addEventListener('click', functionBurger)
-//Burger
-
 
 // Paralaxe
 const translate = document.querySelectorAll(".translate");
---
+++ js/nav.js
@@ -0,0 +1,11 @@
+//Burger
+let burger = document.getElementById('burger');
+let burgerLinks = document.getElementById('burgerLinks');
+
+function functionBurger() {
+    burgerLinks.classList.toggle('menuOpen')
+};
+
+burger.addEventListener('click', functionBurger)
+//Burger
+
